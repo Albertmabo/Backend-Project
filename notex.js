89b3611abf37984c6ed36c863975329e2915e7b6
Albertmabo <albertmabo001@gmail.com>
2024-06-05 19:47:52 +0545

    project setup

---
+++ notex.js
@@ -0,0 +1,34 @@
+/*
+The difference between dependency and dev dependency is that 
+dev dependency are those dependency that we use during the development we don't take it to production.
+
+npm i -D nodemon
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
